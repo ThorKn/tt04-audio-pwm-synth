6f36abd9f4f5ac04abdc1f7b74080ec1932b3fe8
Thorsten Knoll <thorsten_knoll@yahoo.de>
2023-08-25 08:43:09 +0200

    update

--- src/PWMaudio.v
+++ src/PWMaudio.v
@@ -3,9 +3,15 @@
 // Git hash  : 30dc8e87219fd4a3e5c4b952b95d802b49b2c7ea
 
 `timescale 1ns/1ps 
+`default_nettype none
 
 module tt_um_thorkn_pwmaudio (
-  output wire [1:0] uo_out,   // Dedicated outputs
+  input  wire [7:0] ui_in,    // Dedicated inputs
+  output wire [7:0] uo_out,   // Dedicated outputs
+  input  wire [7:0] uio_in,   // IOs: Input path
+  output wire [7:0] uio_out,  // IOs: Output path
+  output wire [7:0] uio_oe,   // IOs: Enable path (active high: 0=input, 1=output
+  input  wire ena,
   input  wire clk,
   input  wire rst_n
 );
