b95f13d0897630efbf815265ec830e433652c0d9
Thorsten Knoll <thorsten_knoll@yahoo.de>
2023-08-25 08:35:27 +0200

    inputs are not pruned?

--- src/PWMaudio.v
+++ src/PWMaudio.v
@@ -5,12 +5,7 @@
 `timescale 1ns/1ps 
 
 module tt_um_thorkn_pwmaudio (
-  input  wire [7:0] ui_in,    // Dedicated inputs
-  output wire [7:0] uo_out,   // Dedicated outputs
-  input  wire [7:0] uio_in,   // IOs: Input path
-  output wire [7:0] uio_out,  // IOs: Output path
-  output wire [7:0] uio_oe,   // IOs: Enable path (active high: 0=input, 1=output
-  input  wire ena,
+  output wire [1:0] uo_out,   // Dedicated outputs
   input  wire clk,
   input  wire rst_n
 );
